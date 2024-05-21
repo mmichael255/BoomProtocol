b58fca661c548c9ca2d58c6b45d75d84fcff7a14
mmichael255 <cby371@gmail.com>
2024-05-21 12:42:13 +0900

    finDepositInInitEnv

--- test/testBoomPoolWithdraw.ts
+++ test/testBoomPoolWithdraw.ts
@@ -3,10 +3,13 @@ import "dotenv/config";
 import { testEnv, initTestEnv } from "./setUpEnv";
 import { it } from "mocha";
 import { ethers } from "hardhat";
+import { Contract, Signer } from "ethers";
 
 const decimals = 18;
 const assetIndex = 1;
-const depolyAmount = ethers.parseEther("10");
+const allowedAmount = ethers.parseEther("15");
+let userDepositedAsset1: Signer = testEnv.users[1];
+let userDepositedAsset2: Signer = testEnv.users[6];
 
 describe("BoomPoolWithdraw", async () => {
   before(async () => {
@@ -15,6 +18,8 @@ describe("BoomPoolWithdraw", async () => {
     await boomPool.addAssert(assets[0]);
     await boomPool.addAssert(assets[1]);
 
+    userDepositedAsset1 = users[1];
+    userDepositedAsset2 = users[6];
     await boomPool.initAssert(
       assets[0],
       priceFeeds[0],
@@ -27,17 +32,47 @@ describe("BoomPoolWithdraw", async () => {
       assets[1],
       priceFeeds[1],
       decimals,
-      assetIndex,
+      assetIndex * 2,
       sTokens[1],
       dTokens[1]
     );
+    const user1Asset1 = assets[0].connect(userDepositedAsset1);
+    const user2Asset2 = assets[1].connect(userDepositedAsset2);
+
+    user1Asset1.approve(boomPool, allowedAmount);
+    user2Asset2.approve(boomPool, BigInt(2) * allowedAmount);
+
+    const user1Pool = boomPool.connect(userDepositedAsset1);
+    const user2Pool = boomPool.connect(userDepositedAsset2);
+
+    await user1Pool.deposit(assets[0], allowedAmount);
+    await user2Pool.deposit(assets[1], BigInt(2) * allowedAmount);
   });
   it("testInitEnv", async () => {
     const { boomPool, assets, users, sTokens, dTokens, priceFeeds } = testEnv;
     //get user balance in assets
-    for (let i = 0; i < users.length; i++) {
-      console.log(await assets[0].balanceOf(users[i]));
-    }
+    console.log(
+      `asset1 user1 balance: ${await assets[0].balanceOf(userDepositedAsset1)}`
+    );
+    console.log(
+      `asset2 user2 balance: ${await assets[1].balanceOf(userDepositedAsset2)}`
+    );
+    console.log(
+      `asset1 stoken1 balance: ${await assets[0].balanceOf(sTokens[0])}`
+    );
+    console.log(
+      `asset2 stoken2 balance: ${await assets[1].balanceOf(sTokens[1])}`
+    );
+    console.log(
+      `stoken1 user1 balance: ${await sTokens[0].balanceOf(
+        userDepositedAsset1
+      )}`
+    );
+    console.log(
+      `stoken2 user2 balance: ${await sTokens[1].balanceOf(
+        userDepositedAsset2
+      )}`
+    );
     // console.log(await boomPool.getAssetInfo(assets[0]));
     // console.log(await boomPool.getAssetInfo(assets[1]));
   });
